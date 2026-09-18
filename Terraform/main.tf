044647692b410e64e6c6e1539c803666f66b6ca7
shaiksaidavali <shaiksaidavali223@gmail.com>
2026-09-18 23:15:08 +0530

    eks-terraform

--- Terraform/main.tf
+++ Terraform/main.tf
@@ -28,28 +28,3 @@ resource "azurerm_network_interface" "example" {
     private_ip_address_allocation = "Dynamic"
   }
 }
-
-resource "azurerm_linux_virtual_machine" "example" {
-  name                            = "${var.prefix}-linux"
-  resource_group_name             = azurerm_resource_group.demo.name
-  location                        = azurerm_resource_group.demo.location
-  size                            = "Standard_B1s"
-  admin_username                  = "adminuser"
-  admin_password                  = var.admin_password
-  disable_password_authentication = false
-  network_interface_ids = [
-    azurerm_network_interface.example.id,
-  ]
-
-  os_disk {
-    caching              = "ReadWrite"
-    storage_account_type = "Standard_LRS"
-  }
-
-  source_image_reference {
-    publisher = "Canonical"
-    offer     = "0001-com-ubuntu-server-jammy"
-    sku       = "22_04-lts"
-    version   = "latest"
-  }
-}
